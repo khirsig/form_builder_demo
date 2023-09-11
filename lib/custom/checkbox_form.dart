0f84c23818735b671827570f364a61f9fa9d3315
Kevin Hirsig <hirsigkevin@gmail.com>
2023-09-11 12:50:20 +0200

    added inputDecoration & fixed tristate

--- lib/custom/checkbox_form.dart
+++ lib/custom/checkbox_form.dart
@@ -3,9 +3,20 @@ import 'package:flutter/material.dart';
 class CheckboxForm extends FormField<bool> {
   CheckboxForm({
     super.key,
+    super.initialValue,
+    super.onSaved,
+    bool tristate = false,
     ValueChanged<bool>? onChanged,
+    InputDecoration decoration = const InputDecoration(
+      border: InputBorder.none,
+      focusedBorder: InputBorder.none,
+      enabledBorder: InputBorder.none,
+      errorBorder: InputBorder.none,
+      disabledBorder: InputBorder.none,
+    ),
   }) : super(
           builder: (FormFieldState<bool> field) {
+            final state = field as _CheckboxFormState;
             void onChangedHandler(bool? value) {
               field.didChange(value);
               if (onChanged != null && value != null) {
@@ -13,12 +24,19 @@ class CheckboxForm extends FormField<bool> {
               }
             }
 
-            final bool value = field.value ?? false;
-
-            return Checkbox(
-              value: value,
-              onChanged: onChangedHandler,
+            return InputDecorator(
+              decoration: decoration,
+              child: Checkbox(
+                value: tristate ? state.value : (state.value ?? false),
+                onChanged: onChangedHandler,
+                tristate: tristate,
+              ),
             );
           },
         );
+
+  @override
+  FormFieldState<bool> createState() => _CheckboxFormState();
 }
+
+class _CheckboxFormState extends FormFieldState<bool> {}
