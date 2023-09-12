913025ca499479a3287e3b29f4f86a029c601a9e
Kevin Hirsig <hirsigkevin@gmail.com>
2023-09-12 08:40:24 +0200

    added focus for checkbox

--- lib/custom/checkbox_form.dart
+++ lib/custom/checkbox_form.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CheckboxForm extends FormField<bool> {
+  final FocusNode? focusNode;
+
   CheckboxForm({
     super.key,
     super.initialValue,
     super.onSaved,
     bool tristate = false,
     ValueChanged<bool>? onChanged,
+    this.focusNode,
     InputDecoration decoration = const InputDecoration(
       border: InputBorder.none,
       focusedBorder: InputBorder.none,
@@ -19,6 +22,7 @@ class CheckboxForm extends FormField<bool> {
             final state = field as _CheckboxFormState;
             void onChangedHandler(bool? value) {
               field.didChange(value);
+              state.focus();
               if (onChanged != null && value != null) {
                 onChanged(value);
               }
@@ -30,6 +34,7 @@ class CheckboxForm extends FormField<bool> {
                 value: tristate ? state.value : (state.value ?? false),
                 onChanged: onChangedHandler,
                 tristate: tristate,
+                focusNode: focusNode,
               ),
             );
           },
@@ -39,4 +44,21 @@ class CheckboxForm extends FormField<bool> {
   FormFieldState<bool> createState() => _CheckboxFormState();
 }
 
-class _CheckboxFormState extends FormFieldState<bool> {}
+class _CheckboxFormState extends FormFieldState<bool> {
+  @override
+  CheckboxForm get widget => super.widget as CheckboxForm;
+
+  late FocusNode effectiveFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    effectiveFocusNode = widget.focusNode ?? FocusNode();
+  }
+
+  void focus() {
+    if (!effectiveFocusNode.hasFocus) {
+      FocusScope.of(context).requestFocus(effectiveFocusNode);
+    }
+  }
+}
